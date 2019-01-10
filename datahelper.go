cb6615fe2101a2c51f02dab873cb5ca158ca820a
Zaldy Baguinon (@IT) <zaldy.baguinon@vdimdci.com.ph>
2019-01-10 09:58:07 +0800

    OK

--- datahelper.go
+++ datahelper.go
@@ -3,44 +3,58 @@ package datahelper
 import (
 	"database/sql"
 	"errors"
-	"log"
 	"strings"
 	"time"
 
-	_ "github.com/denisenkom/go-mssqldb"
+	_ "github.com/denisenkom/go-mssqldb" //SQl Server Driver
 	"github.com/eaglebush/datatable"
+	_ "github.com/mattn/go-sqlite3" //SQlite Driver
 )
 
 // DataHelper struct
 type DataHelper struct {
-	db *sql.DB
-	tx *sql.Tx
+	db         *sql.DB
+	tx         *sql.Tx
+	AllQueryOK bool
+	Errors     []string
+	DriverName string
 }
 
 //NewDataHelper - creates a new DataHelper
-func NewDataHelper() *DataHelper {
-	dh := &DataHelper{}
-	return dh
+func NewDataHelper(driverName string) *DataHelper {
+
+	return &DataHelper{DriverName: driverName}
 }
 
 //Connect - connect to the database
 func (dh *DataHelper) Connect(ConnectionString *string) (bool, error) {
 	var err error
 
-	dh.db, err = sql.Open("mssql", *ConnectionString)
+	dh.db, err = sql.Open(dh.DriverName, *ConnectionString)
 	if err != nil {
-		return false, err
+		return false, errors.New("Connection Error: " + err.Error())
 	}
 
-	err = dh.db.Ping()
-	if err != nil {
-		return false, err
+	if dh.DriverName != "sqlite3" {
+		err = dh.db.Ping()
+		if err != nil {
+			return false, errors.New("Connection Error: " + err.Error())
+		}
 	}
+
+	/*
+		Resets errors and assumes all queries are OK.
+		AllQueryOK is primarily used in a batch of queries.
+		This will be set to false if one of the query execution fails.
+	*/
+	dh.Errors = make([]string, 0)
+	dh.AllQueryOK = true
+
 	return true, nil
 }
 
 //GetData - get data from the database
-func (dh *DataHelper) GetData(preparedQuery string, arg ...interface{}) *datatable.DataTable {
+func (dh *DataHelper) GetData(preparedQuery string, arg ...interface{}) (*datatable.DataTable, error) {
 	dt := datatable.NewDataTable("data")
 
 	var rows *sql.Rows
@@ -48,83 +62,102 @@ func (dh *DataHelper) GetData(preparedQuery string, arg ...interface{}) *datatab
 	if dh.tx != nil {
 		rows, err = dh.tx.Query(preparedQuery, arg...)
 	} else {
+		//If the query is not in a transaction, the following properties are always reset
+		dh.AllQueryOK = true
+		dh.Errors = make([]string, 0)
+
 		rows, err = dh.db.Query(preparedQuery, arg...)
 	}
 
-	if err != nil {
-
-	}
 	defer rows.Close()
 
-	colt, err := rows.ColumnTypes()
+	if err == nil {
+		colt, err := rows.ColumnTypes()
+		if colt != nil && rows != nil {
+			vals := make([]interface{}, len(colt))
 
-	if colt != nil && rows != nil {
-		vals := make([]interface{}, len(colt))
-
-		for i := 0; i < len(colt); i++ {
-			vals[i] = new(interface{})
-			length, _ := colt[i].Length()
-			dt.AddColumn(colt[i].Name(), colt[i].ScanType(), length)
-		}
-
-		r := datatable.Row{}
-
-		for rows.Next() {
-			err = rows.Scan(vals...)
-			if err != nil {
-				continue
+			for i := 0; i < len(colt); i++ {
+				vals[i] = new(interface{})
+				length, _ := colt[i].Length()
+				dt.AddColumn(colt[i].Name(), colt[i].ScanType(), length)
 			}
 
-			r = dt.NewRow()
-			for i := 0; i < len(colt); i++ {
-				v := vals[i].(*interface{})
-				if *v != nil {
-					r.Cells[i].Value = *v
-				} else {
-					t := colt[i].ScanType().Name()
-					switch strings.ToLower(t) {
-					case "bool":
-						r.Cells[i].Value = false
-					case "time":
-						r.Cells[i].Value = time.Time{}
-					case "string":
-						r.Cells[i].Value = ""
-					case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "unit16", "uint32", "unit64":
-						r.Cells[i].Value = 0
-					case "float32", "float64":
-						r.Cells[i].Value = 0.0
-					default:
-						println("Unsupported type")
+			r := datatable.Row{}
+
+			for rows.Next() {
+				err = rows.Scan(vals...)
+				if err != nil {
+					continue
+				}
+
+				r = dt.NewRow()
+				for i := 0; i < len(colt); i++ {
+					v := vals[i].(*interface{})
+					if *v != nil {
+						r.Cells[i].Value = *v
+					} else {
+						t := colt[i].ScanType().Name()
+						switch strings.ToLower(t) {
+						case "bool":
+							r.Cells[i].Value = false
+						case "time":
+							r.Cells[i].Value = time.Time{}
+						case "string":
+							r.Cells[i].Value = ""
+						case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "unit16", "uint32", "unit64":
+							r.Cells[i].Value = 0
+						case "float32", "float64":
+							r.Cells[i].Value = 0.0
+						default:
+							println("Unsupported type")
+						}
 					}
 				}
+				dt.AddRow(r)
 			}
-			dt.AddRow(r)
 		}
+	} else {
+		dh.Errors = append(dh.Errors, err.Error())
+		dh.AllQueryOK = false
 	}
 
-	return dt
+	return dt, err
 }
 
 // Exec - execute queries that does not return rows such us INSERT, DELETE and UPDATE
 func (dh *DataHelper) Exec(preparedQuery string, arg ...interface{}) (sql.Result, error) {
-	var affected sql.Result
+	var result sql.Result
 	var err error
 
 	if dh.tx != nil {
-		affected, err = dh.tx.Exec(preparedQuery, arg...)
-	} else {
-		affected, err = dh.db.Exec(preparedQuery, arg...)
+		result, err = dh.tx.Exec(preparedQuery, arg...)
+
+		if err != nil {
+			dh.AllQueryOK = false
+			dh.Errors = append(dh.Errors, err.Error())
+		}
+
+		return result, err
 	}
 
-	if err != nil {
-		log.Fatal(err)
+	if dh.tx == nil {
+		//If the query is not in a transaction, the following properties are always reset
+		dh.AllQueryOK = true
+		dh.Errors = make([]string, 0)
+
+		return dh.db.Exec(preparedQuery, arg...)
 	}
-	return affected, err
+
+	dh.AllQueryOK = false
+	return nil, errors.New("Unknown execution error")
 }
 
 // Begin - begins a new transaction
 func (dh *DataHelper) Begin() (*sql.Tx, error) {
+	dh.AllQueryOK = true
+
 	tx, err := dh.db.Begin()
+
 	if err != nil {
 		return nil, err
 	}
@@ -137,6 +170,11 @@ func (dh *DataHelper) Commit() error {
 	if dh.tx == nil {
 		return errors.New("No transaction was initiated")
 	}
+
+	//The following properties are always reset after commit
+	dh.AllQueryOK = true
+	dh.Errors = make([]string, 0)
+
 	return dh.tx.Commit()
 }
 
@@ -145,6 +183,11 @@ func (dh *DataHelper) Rollback() error {
 	if dh.tx == nil {
 		return errors.New("No transaction was initiated")
 	}
+
+	//The following properties are always reset after rollback
+	dh.AllQueryOK = true
+	dh.Errors = make([]string, 0)
+
 	return dh.tx.Rollback()
 }
 
--- datahelper_test.go
+++ datahelper_test.go
@@ -5,7 +5,6 @@
 package datahelper
 
 import (
-	"fmt"
 	"log"
 	"testing"
 
@@ -18,32 +17,37 @@ var config cfg.Configuration
 func TestGetData(t *testing.T) {
 	config, _ := cfg.LoadConfig("config.json")
 
-	db := NewDataHelper()
+	db := NewDataHelper(config.DriverName)
 
 	connected, _ := db.Connect(&config.ConnectionString)
 	if connected {
 		defer db.Disconnect()
-		UserKey := 1
-		dt := db.GetData(`SELECT
+		UserKey := 2
+		dt, err := db.GetData(`SELECT
 								UserName, Active, AppsHubAdmin, 
 								DateLastLoggedIn, DisplayName, EmailAddress,
-								ForgotPasswordGUID, GMT, GUID, LDAPLogin, MobileNo, ProfileImageURL
+								ForgotPasswordGUID, GMT, GUID, LDAPLogin, 
+								MobileNo, ProfileImageURL, ActivationCode, ActivationStatus
 						   FROM tshUser WHERE UserKey = ?`, UserKey)
-		if dt.RowCount > 0 {
-			r := dt.Rows[0]
+		if err == nil {
+			if dt.RowCount > 0 {
+				r := dt.Rows[0]
 
-			log.Printf("UserName: %s\r\n", r.Value("UserName").(string))
-			log.Printf("Active: %v\r\n", r.Value("Active"))
-			log.Printf("AppsHubAdmin: %v\r\n", r.Value("AppsHubAdmin"))
-			log.Printf("DateLastLoggedIn: %v\r\n", r.Value("DateLastLoggedIn"))
-			log.Printf("DisplayName: %s\r\n", r.Value("DisplayName"))
-			log.Printf("EmailAddress: %s\r\n", r.Value("EmailAddress"))
-			log.Printf("ForgotPasswordGUID: %s\r\n", r.Value("ForgotPasswordGUID"))
-			log.Printf("GMT: %d\r\n", r.Value("GMT").(int64))
-			log.Printf("GUID: %s\r\n", r.Value("GUID"))
-			log.Printf("LDAPLogin: %v\r\n", r.Value("LDAPLogin"))
-			log.Printf("MobileNo: %s\r\n", r.Value("MobileNo"))
-			log.Printf("ProfileImageURL: %s\r\n", r.Value("ProfileImageURL"))
+				log.Printf("UserName: %s\r\n", r.Value("UserName").(string))
+				log.Printf("Active: %v\r\n", r.Value("Active"))
+				log.Printf("AppsHubAdmin: %v\r\n", r.Value("AppsHubAdmin"))
+				log.Printf("DateLastLoggedIn: %v\r\n", r.Value("DateLastLoggedIn"))
+				log.Printf("DisplayName: %s\r\n", r.Value("DisplayName"))
+				log.Printf("EmailAddress: %s\r\n", r.Value("EmailAddress"))
+				log.Printf("ForgotPasswordGUID: %s\r\n", r.Value("ForgotPasswordGUID"))
+				log.Printf("GMT: %f\r\n", r.Value("GMT").(float64))
+				log.Printf("GUID: %s\r\n", r.Value("GUID"))
+				log.Printf("LDAPLogin: %v\r\n", r.Value("LDAPLogin"))
+				log.Printf("MobileNo: %s\r\n", r.Value("MobileNo"))
+				log.Printf("ProfileImageURL: %s\r\n", r.Value("ProfileImageURL"))
+				log.Printf("ActivationCode: %s\r\n", r.Value("ActivationCode"))
+				log.Printf("ActivationStatus: %s\r\n", r.Value("ActivationStatus"))
+			}
 		}
 	}
 }
@@ -55,12 +59,12 @@ func TestExec(t *testing.T) {
 	connected, _ := db.Connect(&config.ConnectionString)
 	if connected {
 		defer db.Disconnect()
-		r, err := db.Exec(`UPDATE tshUser SET ProfileImageURL=? WHERE UserKey=?`, `https://www.yahoo.com`, 1)
+		r, err := db.Exec(`UPDATE tshUser SET ProfileImageURL=? WHERE UserKey=?`, `https://www.yahoo.com`, 2)
 		if err != nil {
-			println("Error: " + err.Error())
+			log.Printf("Error: " + err.Error())
 		} else {
 			affected, _ := r.RowsAffected()
-			fmt.Printf("Result: %v", affected)
+			log.Printf("Result: %v", affected)
 		}
 	}
 }
@@ -74,14 +78,14 @@ func TestTransactionExec(t *testing.T) {
 		defer db.Disconnect()
 		db.Begin()
 
-		r, err := db.Exec(`UPDATE tshUser SET ProfileImageURL=? WHERE UserKey=?`, `http://www.yahoo.com`, 1)
-		if err != nil {
-			db.Rollback()
-			println("Error: " + err.Error())
-		} else {
+		r, err := db.Exec(`UPDATE tshUser SET ProfileImageURL=? WHERE UserKey=?`, `http://www.yahoo.com`, 2)
+		if db.AllQueryOK {
 			affected, _ := r.RowsAffected()
 			db.Commit()
-			fmt.Printf("Result: %v", affected)
+			log.Printf("Result: %v", affected)
+		} else {
+			db.Rollback()
+			log.Printf("Error: " + err.Error())
 		}
 	}
 }
