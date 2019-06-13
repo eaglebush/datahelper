f85bab1a0c8f8bdce39b94c0a492ddf5779d28a8
Zaldy Baguinon (@IT) <zaldy.baguinon@vdimdci.com.ph>
2019-06-13 18:38:40 +0800

    Datahelper nil

--- datahelper.go
+++ datahelper.go
@@ -367,6 +367,8 @@ func (dh *DataHelper) Prepare(preparedQuery string) (*sql.Stmt, error) {
 
 // Disconnect - disconnect from the database
 func (dh *DataHelper) Disconnect() error {
+	defer func() { dh.db = nil }()
+
 	dh.tx = nil
 
 	if dh.db == nil {
